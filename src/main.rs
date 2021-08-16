6e985a2c773c7885302357292d210c9fe2122626
Felipe Sere <felipesere@gmail.com>
2021-08-16 21:45:17 +0100

    A few more intricate resources

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ pub mod aws {
     use std::fmt;
 
     pub struct Provider {
-        region: String // TODO: Use enums for regions...
+        region: String, // TODO: Use enums for regions...
     }
 
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Default, Clone)]
     pub struct Tags(HashMap<String, String>);
 
     impl Tags {
@@ -30,7 +30,7 @@ pub mod aws {
         relative_id: Option<String>,
     }
 
-    impl <T> fmt::Display for Arn<T> {
+    impl<T> fmt::Display for Arn<T> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             let aws = "aws".into();
             let empty = "".into();
@@ -62,7 +62,9 @@ pub mod aws {
 
     pub mod s3 {
         use crate::aws::{Arn, ArnBuilder, Tags};
+        use crate::iam::PolicyDocument;
         use std::default::Default;
+        use std::rc::Rc;
 
         #[derive(Clone, Debug)]
         pub enum Acl {
@@ -76,11 +78,15 @@ pub mod aws {
             }
         }
 
-        #[derive(Clone, Debug)]
+        #[derive(Builder, Clone, Debug)]
+        #[builder(setter(strip_option, into))]
         pub struct Bucket {
             pub name: String,
+            #[builder(default)]
             pub acl: Acl,
+            #[builder(default)]
             pub website: Option<Website>,
+            #[builder(default)]
             pub tags: Tags,
         }
 
@@ -106,14 +112,127 @@ pub mod aws {
 
         #[derive(Debug, Clone)]
         pub struct Website {
-            index_document: String
+            pub index_document: String,
+        }
+
+        #[derive(Debug)]
+        pub struct BucketObject {
+            bucket: Rc<Bucket>, // TODO: something about id?
+            key: String,
+            content_type: String,
+            content: String,
+        }
+
+        impl BucketObject {
+            pub fn new(
+                bucket: Rc<Bucket>,
+                key: String,
+                content_type: String,
+                content: String,
+            ) -> Self {
+                BucketObject {
+                    bucket,
+                    key,
+                    content_type,
+                    content,
+                }
+            }
+        }
+
+        #[derive(Debug)]
+        pub struct BucketPolicy {
+            pub bucket: Rc<Bucket>,
+            pub policy: PolicyDocument,
+        }
+    }
+
+    pub mod iam {
+        #[derive(Debug)]
+        pub struct PolicyDocument {
+            pub statements: Vec<PolicyStatement>,
+        }
+
+        #[derive(Clone, Debug)]
+        pub enum Effect {
+            Allow,
+            Deny,
+        }
+
+        #[derive(Clone, Debug)]
+        pub enum Principal {
+            AWS(String),
+        }
+
+        #[derive(Clone, Debug)]
+        pub struct Action(String);
+
+        impl Action {
+            pub fn new<S: Into<String>>(action: S) -> Action {
+                Action(action.into())
+            }
+        }
+
+        #[derive(Clone, Debug)]
+        pub struct Resource(String);
+
+        impl Resource {
+            pub fn new<S: Into<String>>(action: S) -> Resource {
+                Resource(action.into())
+            }
+        }
+
+        #[derive(Builder, Debug, Clone)]
+        pub struct PolicyStatement {
+            #[builder(default)]
+            pub sid: String,
+            pub effect: Effect,
+            pub principal: Principal,
+            pub actions: Vec<Action>,
+            pub resources: Vec<Resource>,
         }
     }
 }
 
-fn main() {
-    let x = aws::s3::Bucket::new("my_bucket".into());
-    let arn = x.arn();
+use crate::aws::iam::{self, Action, Effect, Principal, Resource};
+use crate::aws::s3::{self};
+use std::rc::Rc;
 
-    println!("arn: {}", arn);
+fn main() {
+    let bucket = s3::BucketBuilder::default()
+        .name("my_bucket")
+        .website(s3::Website {
+            index_document: "index.html".into(),
+        })
+        .build()
+        .unwrap();
+    let bucket = Rc::new(bucket);
+
+    let index_html = s3::BucketObject::new(
+        bucket.clone(),
+        "index.html".into(),
+        "text/html".into(),
+        "hi this is the content!".into(),
+    );
+
+    let public_can_read = iam::PolicyDocument {
+        statements: vec![iam::PolicyStatementBuilder::default()
+            .effect(Effect::Allow)
+            .principal(Principal::AWS("*".into()))
+            .actions(vec![Action::new("s3:GetObject")])
+            .resources(vec![
+                Resource::new(bucket.arn().to_string()),
+                Resource::new(format!("{}/*", bucket.arn().to_string())),
+            ])
+            .build()
+            .unwrap()],
+    };
+
+    let can_read = s3::BucketPolicy {
+        bucket: bucket.clone(),
+        policy: public_can_read,
+    };
+
+    dbg!(&bucket);
+    dbg!(&index_html);
+    dbg!(&can_read);
 }
