9b71530c4d4501ebebd7c99624d9ae257580e3db
Felipe Sere <felipesere@gmail.com>
2021-08-25 08:15:10 +0100

    Make example smaller for an easier start

--- luminary-bin/src/main.rs
+++ luminary-bin/src/main.rs
@@ -1,4 +1,3 @@
-use aws::iam::{self, Action, Principal, Resource};
 use aws::s3;
 use aws::Arn;
 use std::rc::Rc;
@@ -15,9 +14,6 @@ trait Module: std::fmt::Debug {
 #[derive(Debug)]
 struct MyWebsite {
     bucket: Rc<s3::Bucket>,
-    index_page: s3::BucketObject,
-    public_can_read: Rc<iam::PolicyDocument>,
-    can_read: s3::BucketPolicy,
 }
 
 struct MyWebsiteOutput {
@@ -36,36 +32,11 @@ impl Module for MyWebsite {
             })
             .build()
             .unwrap();
+
         let bucket = Rc::new(bucket);
-
-        let index_page = s3::BucketObject::new(
-            bucket.clone(),
-            "index.html".into(),
-            "text/html".into(),
-            "hi this is the content!".into(),
-        );
-
-        let public_can_read = Rc::new(iam::PolicyDocument {
-            statements: vec![iam::PolicyStatementBuilder::default()
-                .allow()
-                .principal(Principal::AWS("*".into()))
-                .action(Action::new("s3:GetObject"))
-                .resource(Resource::new(bucket.arn().to_string()))
-                .resource(Resource::new(format!("{}/*", bucket.arn().to_string())))
-                .build()
-                .unwrap()],
-        });
-
-        let can_read = s3::BucketPolicy {
-            bucket: bucket.clone(),
-            policy: public_can_read.clone(),
-        };
 
         Self {
             bucket,
-            index_page,
-            public_can_read,
-            can_read,
         }
     }
 
