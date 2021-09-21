ea0b34cf09060858f43ea19492c31a0ff1d6b682
Felipe Sere <felipesere@gmail.com>
2021-09-21 08:39:33 +0100

    Make `map` work on values! Yay!

--- luminary/src/value.rs
+++ luminary/src/value.rs
@@ -14,7 +14,9 @@ impl<T: ?Sized + Clone> Clone for Value<T> {
     }
 }
 
-impl<T: Clone> Value<T> {
+// Will be used for something meaningful down the line
+#[allow(dead_code)]
+impl<T: Clone + 'static> Value<T> {
     fn get(&self) -> T {
         match self {
             Value::Real(ref s) => s.clone(),
@@ -22,31 +24,25 @@ impl<T: Clone> Value<T> {
         }
     }
 
-    /*
-    fn map<F, U>(&self, f: F) -> Value<U>
+    fn map<F, U>(&self, transform: F) -> Value<U>
     where
-        F: Fn(T) -> U,
+        F: 'static + Clone + Fn(T) -> U,
     {
         match self {
-            Value::Real(r) => {
-                Value::Real(f(r.clone()))
-            },
+            Value::Real(real) => Value::Real(transform(real.clone())),
             Value::Reference(producer) => {
-                let other = producer.clone();
+                let step_one = producer.clone();
 
                 Value::Reference(Box::new(move || {
-                    let v = other.get();
-                    f(v)
-                }));
-                todo!()
-            },
+                    let v = step_one.get();
+                    transform(v)
+                }))
+            }
         }
     }
-    */
 }
 
 // We'd have more impls for basic things here
-//
 impl From<String> for Value<String> {
     fn from(content: String) -> Self {
         Value::Real(content)
@@ -56,5 +52,30 @@ impl From<String> for Value<String> {
 impl From<&'static str> for Value<&'static str> {
     fn from(content: &'static str) -> Self {
         Value::Real(content)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_can_map_real_values() {
+        let value = Value::Real(12);
+
+        let transformed = value.map(|v| v + 100);
+
+        assert_eq!(value.get(), 12);
+        assert_eq!(transformed.get(), 112);
+    }
+
+    #[test]
+    fn it_can_map_referenced_value() {
+        let value = Value::Reference(Box::new(|| 12));
+
+        let transformed = value.map(|v| v + 100);
+
+        assert_eq!(value.get(), 12);
+        assert_eq!(transformed.get(), 112);
     }
 }
